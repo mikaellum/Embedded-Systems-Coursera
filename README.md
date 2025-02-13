a6723a3c515916ad5079a8c386677d8a1b34dc97
Michael Asfaw <55643773+mikaellum@users.noreply.github.com>
2025-02-13 07:03:26 -0800

    Initial Submissio with c2 added before solving

--- README.md
+++
@@ -1,6 +0,0 @@
-### Author: Michael Asfaw
-### Date: Feb 11, 2025
-### =====================
-### C code for printing an array and
-### calculating the statistic including 
-### maximum, minimum, mean and median values
---
+++ c1/stats.h
@@ -0,0 +1,98 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file stats.h 
+ * @brief contains declarations for the functions in stats.c
+ *
+ * <Add Extended Description Here>
+ *
+ * @author Michael Asfaw
+ * @date Feb 11, 2025
+ *
+ */
+#ifndef __STATS_H__
+#define __STATS_H__
+
+void print_statistics(unsigned char *arr, int len);
+/**
+ * @brief Prints the Calculated Statistics of the Array
+ *
+ * Takes the return from the other functions and prints them
+ *
+ * @param arr Input Array to the Function
+ * @param len Length of the Array
+ * @return prints the values, returns NULL
+ */
+void print_array(unsigned char *arr, int len);
+/**
+ * @brief Prints the Array input into the function
+ *
+ * Prints the array for display purposes (formatted)
+ *
+ * @param arr Input Array to the Function
+ * @param len Length of the Array
+ * @return prints the values, returns NULL
+ */
+unsigned char find_median(unsigned char *arr, int len);
+/**
+ * @brief Calculates and returns the median value
+ *
+ * Median Value Calculator for an array
+ *
+ * @param arr Input Array to the Function
+ * @param len Length of the Array
+ * @return median value
+ */
+unsigned char find_mean(unsigned char *arr, int len);
+/**
+ * @brief Calculates and returns the mean value
+ *
+ * Mean Value Calculator for an array
+ *
+ * @param arr Input Array to the Function
+ * @param len Length of the Array
+ * @return mean value
+ */
+unsigned char find_maximum(unsigned char *arr, int len);
+/**
+ * @brief Calculates and returns the maximum value
+ *
+ * Maximum Value Calculator for an array
+ *
+ * @param arr Input Array to the Function
+ * @param len Length of the Array
+ * @return max value
+ */
+unsigned char find_minimum(unsigned char *arr, int len);
+/**
+ * @brief Calculates and returns the minimum value
+ *
+ * Minimum Value Calculator for an array
+ *
+ * @param arr Input Array to the Function
+ * @param len Length of the Array
+ * @return min value
+ */
+unsigned char * sort_array(unsigned char *arr, int len);
+/**
+ * @brief Sorts the array and returns a sorted Array
+ *
+ * Sorts the Array and returns a new sorted array
+ *
+ * @param arr Input Array to the Function
+ * @param len Length of the Array
+ * @return sortArr Sorted Array
+ */
+
+
+
+
+#endif /* __STATS_H__ */
